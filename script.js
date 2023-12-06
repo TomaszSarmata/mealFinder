226da1ccbea965bcebae1436e7b48c7fa25858c5
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2023-12-06 16:17:12 +0000

    added event listerner to our form and implemented part of the searchMeal function that will be fetching data from the third party api

--- script.js
+++ script.js
@@ -4,3 +4,44 @@ const search = document.getElementById("search"),
   mealsEl = document.getElementById("meals"),
   resultHeading = document.getElementById("result-heading"),
   single_mealEl = document.getElementById("single-meal");
+
+//Helper Functions
+
+//search meal and fetch from api
+
+function searchMeal(e) {
+  e.preventDefault();
+
+  //Clear single meal
+  single_mealEl.innerHTML = "";
+
+  //Get search term
+  const term = search.value;
+
+  //Check if term empty
+  if (term.trim()) {
+  } else {
+    alert("please type in the name of the meal");
+  }
+  console.log(term);
+}
+
+// async function searchMeal(e) {
+//   e.preventDefault();
+//   const response = await fetch(
+//     "https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata"
+//   );
+//   const data = await response.json();
+//   console.log(data.meals[0].strMealThumb);
+//   updateUI(data);
+// }
+
+// function updateUI(data) {
+//   let mealName = data.meals[0].strMeal;
+//   resultHeading.innerText = mealName;
+//   let mealImgUrl = data.meals[0].strMealThumb;
+//   mealsEl.innerHTML = `<img src="${mealImgUrl}"/> <img src="${mealImgUrl}"/> <img src="${mealImgUrl}"/> <img src="${mealImgUrl}"/>`;
+// }
+
+//Event Listeners
+submit.addEventListener("submit", searchMeal);
